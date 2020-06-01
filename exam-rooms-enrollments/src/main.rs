62ae0b2c68b35671b55a5cf3179faad2df266c2a
Carlos Saito <carlos@saito.style>
2020-06-01 15:15:41 +0200

    Fmt

--- exam-rooms-enrollments/src/main.rs
+++ exam-rooms-enrollments/src/main.rs
@@ -1,9 +1,10 @@
-extern crate pretty_env_logger;
 extern crate dotenv;
-#[macro_use] extern crate log;
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
 use dotenv::dotenv;
+use serde::Deserialize;
 use std::env;
-use serde::{Deserialize};
 
 fn env(key: &str) -> String {
     match env::var(key) {
@@ -17,24 +18,23 @@ fn env(key: &str) -> String {
 
 #[derive(Deserialize, Debug)]
 struct Course {
-    sis_course_id: Option<String>
+    sis_course_id: Option<String>,
 }
 
-async fn get_courses (account_id: &str) -> Result<Vec<Course>, reqwest::Error> {
+async fn get_courses(account_id: &str) -> Result<Vec<Course>, reqwest::Error> {
     let canvas_url = env("CANVAS_API_URL");
     let canvas_token = env("CANVAS_API_TOKEN");
 
     let client = reqwest::Client::new();
-    let response = client.get(&format!("{}/accounts/{}/courses", canvas_url, account_id))
+    let response = client
+        .get(&format!("{}/accounts/{}/courses", canvas_url, account_id))
         .bearer_auth(canvas_token)
         .send()
         .await?;
 
     println!("{:?}", response.headers().get("link"));
 
-    response
-        .json::<Vec<Course>>()
-        .await
+    response.json::<Vec<Course>>().await
 }
 
 #[tokio::main]
