870d1fdf01a2f2f932db6018735494cd4a8a2b4c
Carlos Saito <carlos@saito.style>
2020-06-02 16:25:35 +0200

    Write in CSV file

--- enrollments_2020_p3_p4/src/main.rs
+++ enrollments_2020_p3_p4/src/main.rs
@@ -1,11 +1,14 @@
 extern crate dotenv;
 
+use csv::Writer;
 use dotenv::dotenv;
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 use std::env;
+extern crate canvas_api;
+use canvas_api::{CanvasApi};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct CourseRound {
     course_code: String,
     first_semester: String,
@@ -16,13 +19,45 @@ struct CourseRound {
     offered_semesters: Vec<OfferedSemester>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone)]
 struct OfferedSemester {
     start_date: String,
     end_date: String,
     start_week: String,
     end_week: String,
     semester: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Section {
+    course_id: i32,
+    sis_section_id: Option<String>,
+    sis_course_id: Option<String>,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct Enrollment {
+    id: i32,
+    sis_user_id: Option<String>,
+    sis_section_id: Option<String>,
+    role: String,
+    user: User,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct User {
+    sortable_name: Option<String>,
+    login_id: Option<String>,
+}
+
+#[derive(Serialize)]
+struct Row<'a> {
+    name: &'a str,
+    course: &'a str,
+    role: &'a str,
+    section: &'a str,
+    mail1: &'a str,
+    mail2: &'a str,
 }
 
 #[derive(Serialize)]
@@ -34,10 +69,26 @@ struct OutputRow {
     period: String,
 }
 
+fn valid_offered_semesters(all_semesters: Vec<OfferedSemester>) -> Vec<OfferedSemester> {
+    let mut result = vec![];
+
+    for semester in all_semesters.into_iter() {
+        if semester.semester == "20201" && semester.end_date >= "2020-03-20".to_string() {
+            result.push(semester);
+        }
+    }
+
+    return result;
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+
     let kopps_url = env::var("KOPPS_API_URL").unwrap();
-    env::var("CANVAS_API_URL").unwrap();
+    let canvas_api = CanvasApi::new(
+        env::var("CANVAS_API_URL").unwrap(),
+        env::var("CANVAS_API_TOKEN").unwrap(),
+    );
     let client = Client::new();
 
     println!("Calling Kopps API to get course rounds");
@@ -51,17 +102,65 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let total_length = course_rounds.len();
 
-    let filtered: Vec<CourseRound> = course_rounds
+    let filtered = course_rounds
         .into_iter()
         .filter(|round| round.state == "Godkänt")
         .filter(|round| round.first_period == "20201P3" || round.first_period == "20201P4")
-        .collect();
+        .map(|round| CourseRound {
+            offered_semesters: valid_offered_semesters(round.offered_semesters),
+            ..round
+        })
+        .filter(|round| round.offered_semesters.len() > 0);
 
     println!(
         "Course rounds in 2020-P3/P4 {}. Total course rounds in 2020-VT {}",
-        filtered.len(),
+        filtered.clone().count(),
         total_length
     );
+
+    let mut wtr = Writer::from_path("list.csv")?;
+
+    for round in filtered {
+        let sis_id = format!(
+            "{}{}{}",
+            round.course_code, round.first_semester, round.offering_id
+        );
+
+        let pages =
+            canvas_api.get_paginated(&format!("/sections/sis_section_id:{}/enrollments", sis_id));
+
+        for response in pages {
+            let result = response?;
+            let status = &result.status();
+
+            if status == &404 {
+                println!("Section {} not found", sis_id);
+                break;
+            } else if status != &200 {
+                println!(
+                    "Unexpected response when requesting section '{}'. Status: {}",
+                    sis_id, status
+                );
+                panic!();
+            }
+
+            let enrollments = result.json::<Vec<Enrollment>>()?.into_iter();
+
+            for enrollment in enrollments {
+                wtr.serialize(Row {
+                    course: &format!("{} {}", sis_id, round.first_period),
+                    name: &enrollment.user.sortable_name.unwrap_or("??".to_string()),
+                    role: &enrollment.role,
+                    section: &enrollment.sis_section_id.unwrap_or("??".to_string()),
+                    mail1: &format!(
+                        "{}@kth.se",
+                        enrollment.sis_user_id.unwrap_or("??".to_string())
+                    ),
+                    mail2: &enrollment.user.login_id.unwrap_or("??".to_string()),
+                })?;
+            }
+        }
+    }
 
     /*
     let mut wtr = Writer::from_path("foo.csv")?;
