71626c65ff727223466dc954b01059f34ecb1993
Carlos Saito <carlos@saito.style>
2021-05-25 17:05:45 +0200

    Cleanup

---
+++ list_enrollments/src/akt_api.rs
@@ -0,0 +1,43 @@
+use chrono::NaiveDate;
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use url::Url;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Response {
+    pub aktivitetstillfallen: Vec<Aktivitetstillfalle>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Aktivitetstillfalle {
+    #[serde(rename = "ladokUID")]
+    pub ladok_uid: String,
+}
+
+/// Get all the course rounds in a given term that starts in a given period
+pub fn get_aktivitetstillfallen(
+    akt_url: &str,
+    akt_token: &str,
+    date: &NaiveDate,
+) -> impl Iterator<Item = Aktivitetstillfalle> {
+    let url_with_slash = match akt_url.ends_with("/") {
+        true => akt_url.to_string(),
+        false => format!("{}/", akt_url),
+    };
+
+    let suffix = format!(
+        "aktivitetstillfallen/students?fromDate={}&toDate={}",
+        date, date
+    );
+    let full_url = Url::parse(&url_with_slash).unwrap().join(&suffix).unwrap();
+
+    Client::new()
+        .get(full_url)
+        .header("canvas_api_token", akt_token)
+        .send()
+        .unwrap()
+        .json::<Response>()
+        .unwrap()
+        .aktivitetstillfallen
+        .into_iter()
+}
--- list_enrollments/src/main.rs
+++ list_enrollments/src/main.rs
@@ -1,7 +1,9 @@
+mod akt_api;
 mod canvas_api;
 mod kopps_api;
+use chrono::NaiveDate;
 use csv::Writer;
-use dialoguer::{theme::ColorfulTheme, Input, Select};
+use dialoguer::{theme::ColorfulTheme, Input, Password, Select};
 use dotenv::dotenv;
 use serde::Serialize;
 use std::env;
@@ -19,6 +21,16 @@ struct Row<'a> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
+    let option = prompt_choice();
+
+    if option == 0 {
+        list_course_room_enrollments()
+    } else {
+        list_exam_room_enrollments()
+    }
+}
+
+fn list_course_room_enrollments() -> Result<(), Box<dyn std::error::Error>> {
     let kopps_api_url = env("KOPPS_API_URL");
     let canvas_api_url = env("CANVAS_API_URL");
     let canvas_api_token = env("CANVAS_API_TOKEN");
@@ -51,6 +63,62 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ),
                 mail2: &enrollment.user.login_id.unwrap_or("??".to_string()),
             })?;
+        }
+    }
+
+    Ok(())
+}
+
+fn list_exam_room_enrollments() -> Result<(), Box<dyn std::error::Error>> {
+    let canvas_api_url = env("CANVAS_API_URL");
+    let canvas_api_token = env("CANVAS_API_TOKEN");
+
+    let akt_api_url = env("AKTIVITETSTILLFALLEN_API_URL");
+    let akt_api_token = prompt_akt_token();
+
+    let start_date = prompt_date("Enter the start date");
+    let end_date = prompt_date("Enter the end date");
+
+    let file_path = format!("enrollments-courserooms-{}---{}.csv", start_date, end_date);
+    let mut wtr = Writer::from_path(file_path)?;
+
+    for date in start_date
+        .iter_days()
+        .take((end_date - start_date).num_days() as usize + 1)
+    {
+        println!("Getting activities for {}", date);
+        let aktivitetstillfallen =
+            akt_api::get_aktivitetstillfallen(&akt_api_url, &akt_api_token, &date);
+
+        for round in aktivitetstillfallen {
+            println!("- Activity {}", &round.ladok_uid);
+            let sis_id1 = format!("AKT.{}", &round.ladok_uid);
+            let sis_id2 = format!("AKT.{}.FUNKA", &round.ladok_uid);
+            let mut enrollments = canvas_api::get_enrollments(
+                canvas_api_url.clone(),
+                canvas_api_token.clone(),
+                &sis_id1,
+            )?;
+
+            enrollments.append(&mut canvas_api::get_enrollments(
+                canvas_api_url.clone(),
+                canvas_api_token.clone(),
+                &sis_id2,
+            )?);
+
+            for enrollment in enrollments.into_iter() {
+                wtr.serialize(Row {
+                    course: &sis_id1,
+                    name: &enrollment.user.sortable_name.unwrap_or("??".to_string()),
+                    role: &enrollment.role,
+                    section: &enrollment.sis_section_id.unwrap_or("??".to_string()),
+                    mail1: &format!(
+                        "{}@kth.se",
+                        enrollment.sis_user_id.unwrap_or("??".to_string())
+                    ),
+                    mail2: &enrollment.user.login_id.unwrap_or("??".to_string()),
+                })?;
+            }
         }
     }
 
@@ -101,4 +169,57 @@ fn prompt_year_term_period() -> (String, String) {
     let period = periods[period_selection];
 
     (format!("{}{}", year, term), period.to_string())
+}
+
+fn prompt_akt_token() -> String {
+    Password::with_theme(&ColorfulTheme::default())
+        .with_prompt("Paste the Aktivitetstillfällen API token")
+        .interact()
+        .expect("Failed to prompt akt token")
+}
+
+fn prompt_choice() -> usize {
+    let options = vec![
+        "Course room enrollments".to_string(),
+        "Exam room enrollments".to_string(),
+    ];
+
+    Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("What do you want to do?")
+        .items(&options)
+        .default(0)
+        .interact()
+        .expect("Failed")
+}
+
+fn prompt_date(prompt: &str) -> NaiveDate {
+    let date_str: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .interact_text()
+        .expect("Failed to prompt date");
+
+    let values: Vec<&str> = date_str
+        .split("-")
+        // .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+
+    let (year, month, day) = (
+        values
+            .get(0)
+            .expect("Cannot get year")
+            .parse::<i32>()
+            .unwrap(),
+        values
+            .get(1)
+            .expect("Cannot get month")
+            .parse::<u32>()
+            .unwrap(),
+        values
+            .get(2)
+            .expect("Cannot get day")
+            .parse::<u32>()
+            .unwrap(),
+    );
+
+    NaiveDate::from_ymd(year, month, day)
 }
